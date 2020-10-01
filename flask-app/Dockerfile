7b7386b543cf893cf40b6053b9548cf8cd439003
hassanraja2 <rajahassan42@hotmail.co.uk>
2020-10-01 13:03:23 +0100

    adding scripts

---
+++ flask-app/Dockerfile
@@ -0,0 +1,5 @@
+FROM python:3.7
+COPY . .
+RUN pip install flask
+EXPOSE 5000
+ENTRYPOINT ["python", "app.py"]
---
+++ run.sh
@@ -0,0 +1,2 @@
+#1 /bin/bash
+sudo docker run -d -p 5000:5000 --name flask-app-demo2 localhost:8082/flask-app-demo2
